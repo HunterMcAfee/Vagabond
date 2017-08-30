ae962bc0bc62a13c34c5bb35fb3e4c1cb7ebf7b7
kgreen14 <kgreen14@gmail.com>
2017-08-30 16:16:21 -0400

    Created partial for post, created new.html.erb for posts' view. Can create new posts under each city.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,26 @@
 class PostsController < ApplicationController
+    def city
+     City.find(params[:city_id])
+    end
+    
     def show
         
     end
+
+    def new
+        @city = city
+        @post = city.posts.new
+    end
+
+    def create
+        @post = city.posts.create!(post_params)
+
+        redirect_to city_post_path(city, @post)
+    end
+
+    private
+    def post_params
+       params.require(:post).permit(:title, :content) 
+    end
+
 end
