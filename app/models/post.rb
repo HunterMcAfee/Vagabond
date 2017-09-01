134031a5f5d97a7c45fef610d9a28181ddb2815a
HunterMcAfee <huntmac10@gmail.com>
2017-09-01 11:44:09 -0400

    Added users model and views through devise.  Updated post table and model to include the user.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { minium: 1, maximum: 200 }
   validates :content, presence: true
-  belongs_to :city
+  belongs_to :city, :user
 end
